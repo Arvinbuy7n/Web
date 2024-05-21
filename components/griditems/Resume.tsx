30a9a974c2cc298a03c34bc8e285f4b4f3f78ad9
Arvinbuy7n <chekist.ot@gmail.com>
2024-05-21 12:31:47 +0800

    resume

--- components/griditems/Resume.tsx
+++ components/griditems/Resume.tsx
@@ -9,9 +9,7 @@ export const Resume = () => {
       className="dark:bg-slate-700 bg-[#F6F2F2] rounded-[20px]  max-[481px]:text-sm max-[481px]:w-[140px] max-[481px]:h-[60px] py-3 px-6 lg:text-lg shadow-lg"
       onMouseDown={(e: any) => {
         e.stopPropagation();
-        window.open(
-          "https://www.figma.com/design/u66RwlyQAFmU2sFUc2HPvR/Untitled?node-id=0-1&t=mcm1oe6r5jX0khlz-0"
-        );
+        window.open("/untitled.pdf");
       }}
     >
       Click to review my CV
